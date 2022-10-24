35e25b563a48dd9e841ed96097214a244d9f93ad
paco <admin@md-online.info>
2022-10-24 06:07:10 -0500

    2nda version

--- training.js
+++ training.js
@@ -9,3 +9,23 @@ console.log(number);
 let con = "manita";
 
 const pi = 3.14;
+
+
+//Operaciones matematicas
+
+var sum = 10 + 9;
+
+var mul = 5 * 20;
+
+var div = 200 / 5;
+
+var ress = 40 - 20;
+
+
+console.log(sum);
+
+console.log(mul);
+
+console.log(div);
+
+console.log(ress);
